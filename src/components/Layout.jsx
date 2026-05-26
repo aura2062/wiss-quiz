379e171b7f35d10de305f816f54ba3a6896e2cc1
aura2062 <aura_2062@proton.me>
2026-05-26 10:31:25 +0200

    fixed whitespace v1

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "./Navigation";
-import Footer from "./Footer"
+import Footer from "./Footer";
+
 function Layout() {
     return (
         <div className="App">
