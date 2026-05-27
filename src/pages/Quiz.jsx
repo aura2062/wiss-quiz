f4f42baa574212b69674e884c007cdcf1b779285
aura2062 <aura_2062@proton.me>
2026-05-27 11:07:26 +0200

    deleted unnecessary import in Quiz.jsx

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,6 +1,5 @@
 import Question from "../components/Question";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 
 const questions = [{
